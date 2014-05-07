872c08bc09f0b36bdc394c13ea3757a045b70d73
arturor <arturor@mail.ru>
2014-05-07 13:40:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+        /*VelocityMap *map = new VelocityMap(19,2,true);
+    */
 
     return a.exec();
 }
