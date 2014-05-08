4192a3e226e6903c359ff7226fcb462df69c83a6
arturor <arturor@mail.ru>
2014-05-07 23:25:07 -0700

    Update VelocityMap.h

--- VelocityMap.h
+++ VelocityMap.h
@@ -12,23 +12,23 @@ private:
 
     unsigned int XIndex;   // размеры массива - строки
     unsigned int YIndex;   //                 - столбцы
-    double **Velocities;   // массив, где будем хранить карту
+    double **Pos_Vel;   // массив, где будем хранить карту
 
 public:
 
-    VelocityMap(unsigned int n,unsigned int m, bool IsFromFile);
-    ~VelocityMap();
+    VelocityMap(unsigned int n,unsigned int m, bool IsFromFile);     // устанавливаем размер массива n x m, говорим: с файла ли данные
+    ~VelocityMap();                                                 // деструктор
 
-    double GetNumber(unsigned int n, unsigned int m);
-    void SetNumber(unsigned int n, unsigned int m, double Var);
+    double GetNumber(unsigned int n, unsigned int m);                // дает Pos_Vel[n][m]
+    void SetNumber(unsigned int n, unsigned int m, double Var);     // устанавливает Pos_Vel[n][m] = Val
 
     double* operator[](unsigned int n);
 
-    void Load_Stream(string FileName);
-    void Save_Stream(string FileName);
+    void Load_Stream(string FileName);                               // загрузка   с файла FileName
+    void Save_Stream(string FileName);                              // сохранение в
 
-    double* Get_Velocity_At(double x, double y);
-    double* Calculate_Force(double x, double y);
+    double* Get_Velocity_At(double x, double y);                    // интерполяция
+    double* Calculate_Force(double x, double y);                    // расчет силы давления воды в точке (x,y)
 
 };
 
